a6888172fcca37e58c62ce32231cd6fe8c53b3e4
https://github.com/MohdZeeshan009C <https://github.com/MohdZeeshan009>
2023-04-29 11:54:46 +0530

    adding post and double check

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,30 @@
-module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
-}
 
+const post = require('../models/post');
+module.exports.home = function (req, res){
+
+    // post.find({}, function(err, posts){
+    //     if(err){
+    //         console.log('Error in showing post'); return};
+
+    //         return res.render('home', {
+    //             title: 'Codeial | Home',
+    //             posts: posts
+    //         })
+    // })
+
+
+// Populate data 
+    post.find({}).populate('user').exec(function(err, posts){
+        if(err){
+            console.log('Error in showing user post');
+        }
+
+        return res.render('home', {
+            title : 'Home | Codieal',
+            posts : posts,
+        });
+    });
+
+
+}
 // module.exports.actionName = function(req, res){}
