c0195854bb3b9a4be5415ba9bd37355af52df046
https://github.com/MohdZeeshan009C <https://github.com/MohdZeeshan009>
2023-05-14 08:07:30 +0530

    Adding gulp and rotate-file-stream to manage our css js files

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,9 +1,7 @@
-
 const Post = require('../../../models/post');
 const Comment = require('../../../models/comment');
+module.exports.index = async function(req, res){
 
-
-module.exports.index = async function (req, res) {
 
     let posts = await Post.find({})
         .sort('-createdAt')
@@ -18,34 +16,36 @@ module.exports.index = async function (req, res) {
     return res.json(200, {
         message: "List of posts",
         posts: posts
-    });
+    })
 }
 
 
-module.exports.destroy = async function (req, res) {
+module.exports.destroy = async function(req, res){
 
-    try {
+    try{
         let post = await Post.findById(req.params.id);
 
-        if (post.user == req.user.id) {
+        if (post.user == req.user.id){
             post.remove();
 
-            await Comment.deleteMany({ post: req.params.id });
+            await Comment.deleteMany({post: req.params.id});
 
+
+    
             return res.json(200, {
                 message: "Post and associated comments deleted successfully!"
             });
-
-        } else {
+        }else{
             return res.json(401, {
-                message: "You cannot delete this post!!!"
+                message: "You cannot delete this post!"
             });
         }
-    } catch (err) {
-        console.log("********", err);
+
+    }catch(err){
+        console.log('********', err);
         return res.json(500, {
-            message: "Internal server error"
+            message: "Internal Server Error"
         });
     }
+    
 }
-
